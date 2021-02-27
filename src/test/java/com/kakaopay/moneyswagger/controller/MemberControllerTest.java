6dfefd78c95f3468a64b7bc5ee4a70d7a39c590e
yx3ssi3fil <yx3ssi3fil@gmail.com>
2021-02-28 00:04:50 +0900

    refactor(member): refactor test codes to create member

--- src/test/java/com/kakaopay/moneyswagger/controller/MemberControllerTest.java
+++ src/test/java/com/kakaopay/moneyswagger/controller/MemberControllerTest.java
@@ -2,6 +2,7 @@ package com.kakaopay.moneyswagger.controller;
 
 import com.kakaopay.moneyswagger.AbstractControllerTest;
 import com.kakaopay.moneyswagger.dto.CreateMemberDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -9,23 +10,21 @@ import org.springframework.http.MediaType;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberControllerTest extends AbstractControllerTest {
+    private MemberHttpTest memberHttpTest;
+
+    @BeforeEach
+    public void setUp() {
+        this.memberHttpTest = new MemberHttpTest(webTestClient);
+    }
+
     @DisplayName("회원가입")
     @Test
     void signUp() {
         //given
-        CreateMemberDto.Request requestBody = new CreateMemberDto.Request("name");
+        String name = "name";
 
         //when
-        CreateMemberDto.Response responseBody = webTestClient
-                .post().uri(MemberController.URL_MEMBERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .bodyValue(requestBody)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody(CreateMemberDto.Response.class)
-                .returnResult()
-                .getResponseBody();
+        CreateMemberDto.Response responseBody = memberHttpTest.createMember(name);
 
         //then
         assertThat(responseBody.getId()).isNotNull().isNotNegative();
---
+++ src/test/java/com/kakaopay/moneyswagger/controller/MemberHttpTest.java
@@ -0,0 +1,28 @@
+package com.kakaopay.moneyswagger.controller;
+
+import com.kakaopay.moneyswagger.dto.CreateMemberDto;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+public class MemberHttpTest {
+    private WebTestClient webTestClient;
+
+    public MemberHttpTest(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
+
+    public CreateMemberDto.Response createMember(String name) {
+        CreateMemberDto.Request requestBody = new CreateMemberDto.Request("name");
+
+        return webTestClient
+                .post().uri(MemberController.URL_MEMBERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(CreateMemberDto.Response.class)
+                .returnResult()
+                .getResponseBody();
+    }
+}
