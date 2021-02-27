b28cff04c0ef8339b762c9185403f25586d51138
yx3ssi3fil <yx3ssi3fil@gmail.com>
2021-02-27 19:50:48 +0900

    chore: initial commit

---
+++ src/main/java/com/kakaopay/moneyswagger/MoneyswaggerApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.moneyswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyswaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakaopay/moneyswagger/MoneyswaggerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kakaopay.moneyswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyswaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
