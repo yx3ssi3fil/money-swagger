9f46ee4592fe9530fce9eadd68db72ebd8e5ad0e
yx3ssi3fil <yx3ssi3fil@gmail.com>
2021-02-28 01:13:56 +0900

    refactor(account): refactor test to create account

--- src/test/java/com/kakaopay/moneyswagger/controller/AccountControllerTest.java
+++ src/test/java/com/kakaopay/moneyswagger/controller/AccountControllerTest.java
@@ -5,16 +5,17 @@ import com.kakaopay.moneyswagger.dto.CreateAccountDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountControllerTest extends AbstractControllerTest {
+    private AccountHttpTest accountHttpTest;
     private MemberHttpTest memberHttpTest;
     private Long memberId;
 
     @BeforeEach
     void setUp() {
+        accountHttpTest = new AccountHttpTest(webTestClient);
         memberHttpTest = new MemberHttpTest(webTestClient);
         memberId = memberHttpTest.createMember("name").getId();
     }
@@ -22,19 +23,8 @@ public class AccountControllerTest extends AbstractControllerTest {
     @DisplayName("계좌 생성")
     @Test
     void createAccount() {
-        //given
-        CreateAccountDto.Request requestBody = new CreateAccountDto.Request(memberId);
-
         //when
-        CreateAccountDto.Response responseBody = webTestClient.post().uri(AccountController.URL_CREATE_ACCOUNTS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .bodyValue(requestBody)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody(CreateAccountDto.Response.class)
-                .returnResult()
-                .getResponseBody();
+        CreateAccountDto.Response responseBody = accountHttpTest.createAccount(memberId);
 
         //then
         assertThat(responseBody.getAccountId()).isNotNull().isNotNegative();
---
+++ src/test/java/com/kakaopay/moneyswagger/controller/AccountHttpTest.java
@@ -0,0 +1,27 @@
+package com.kakaopay.moneyswagger.controller;
+
+import com.kakaopay.moneyswagger.dto.CreateAccountDto;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+public class AccountHttpTest {
+    private WebTestClient webTestClient;
+
+    public AccountHttpTest(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
+
+    public CreateAccountDto.Response createAccount(Long memberId) {
+        CreateAccountDto.Request requestBody = new CreateAccountDto.Request(memberId);
+
+        return webTestClient.post().uri(AccountController.URL_CREATE_ACCOUNTS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(CreateAccountDto.Response.class)
+                .returnResult()
+                .getResponseBody();
+    }
+}
